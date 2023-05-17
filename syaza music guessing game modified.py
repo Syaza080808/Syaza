64c6d746d26c80c9ea82e760f8719b889375ac49
Syaza080808 <133829862+Syaza080808@users.noreply.github.com>
2023-05-17 11:35:58 +0800

    Add files via upload

--- syaza music guessing game modified.py
+++ syaza music guessing game modified.py
@@ -4,14 +4,14 @@ import random
 
 def main():
  """Start guessing the name of song (2023 era only)"""
- print("Guess the name of song and the singer (2023 era only gurlll)")
+ print("Guess the name of song, the singer  and genre(2023 era only gurlll)")
 
- x = str("In ha mood by ice spice")
+ x = str("In ha mood by ice spice pop genre")
  guess = None
 
  while x != guess:
 
-    guess = str(input("guess a 2023 era song: "))
+    guess = str(input("guess a 2023 era song, singer and genre: "))
 
     if x == guess:
         print("You deserve to be a gen-z!")
